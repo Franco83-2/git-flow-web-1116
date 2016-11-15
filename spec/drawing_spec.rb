81faa9280ccbc7cedfca6b9c17ba7904704aed6a
Franco Cappitta <franco84@gmail.com>
2016-11-15 02:09:17 +0000

    Done.

--- spec/drawing_spec.rb
+++ spec/drawing_spec.rb
@@ -1,11 +1,12 @@
+
 describe "drawing" do
   it "a file called drawing exists in the public/img folder" do
     file_names = Dir["public/img/*"]
-    expect(file_names).to include("public/img/drawing.png")
+    expect(file_names).to include("public/img/edit.png")
   end
 
   it "the drawing.png file is not empty" do
-    contents = read_file("public/img/drawing.png")
+    contents = read_file("public/img/edit.png")
     expect(contents.length).to be > 500
   end
 
